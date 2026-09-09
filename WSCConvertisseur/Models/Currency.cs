bf081f8f18ae0c46da4c3703306b651919ff33ab
Kateryna Pavlichenko <Kateryna.Pavlichenko@etu.univ-smb.fr>
2026-09-09 19:53:52 +0300

    Refactor: rename Devise model and attributes to Currency

---
+++ WSCConvertisseur/Models/Currency.cs
@@ -0,0 +1,23 @@
+﻿namespace WSCConvertisseur.Models
+{
+    public class Currency
+    {
+        public Currency(int id, string currencyName, double rate)
+        {
+            Id = id;
+            CurrencyName = currencyName;
+            Rate = rate;
+        }
+
+        public Currency() {
+            Id = 0;
+            CurrencyName = string.Empty;
+            Rate = 0.0;
+        }
+
+
+        public int Id { get; set; }
+        public string CurrencyName { get; set; }
+        public double Rate { get; set; }
+    }
+}
